48dcfaba159b4de3cccbd6e838e1bae338766a60
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-20 19:42:43 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,16 +59,17 @@ def generate_random_dates(start_date, end_date, id_values):
     return np.random.choice(date_range, size=len(id_values), replace=False)
 
 ## Generate 100 rows of data
-data = {'ID': id_values,
-        'Issue': issue_list,
+data = {'Issue': issue_list,
         'Status': np.random.choice(['Open', 'In Progress', 'Closed'], size=100),
         'Priority': np.random.choice(['High', 'Medium', 'Low'], size=100),
         'Date': generate_random_dates(start_date, end_date, id_values)
     }
+df = pd.DataFrame(data)
+df = df.insert(0, 'ID', id_values) 
 
 ## Create DataFrame
 if 'df' not in st.session_state:
-    st.session_state.df = pd.DataFrame(data)
+    st.session_state.df = df
 
 # Tabs for app layout
 tabs = st.tabs(['Write a ticket', 'Ticket Status and Analytics'])
