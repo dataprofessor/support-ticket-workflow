66d84f79630dd7d13e4137e84791b67c1d85528e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-20 11:37:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,13 +8,7 @@ tabs = (['Write a ticket', 'Ticket Status and Analytics'])
 with tabs[0]:
   st.write('File a new ticket')
 
-  with st.form('addition'):
-    a = st.number_input('a')
-    b = st.number_input('b')
-    submit = st.form_submit_button('add')
 
-  if submit:
-      col2.title(f'{a+b:.2f}')
 
 with tabs[1]:
   st.write('Check the status of your ticket')
