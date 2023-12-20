b6cf51b82ab6a029c0f45a4be9dd1d93de4a5c23
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-20 16:40:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,48 @@ st.markdown("""
 </style>
 """, unsafe_allow_html=True)
 
+# Generate data
+## Set seed for reproducibility
+np.random.seed(42)
+
+## Function to generate a random issue description
+def generate_issue():
+    issues = [
+        "Network connectivity issues in the office",
+        "Software application crashing on startup",
+        "Printer not responding to print commands",
+        "Email server downtime",
+        "Data backup failure",
+        "Login authentication problems",
+        "Website performance degradation",
+        "Security vulnerability identified",
+        "Hardware malfunction in the server room",
+        "Employee unable to access shared files",
+        "Database connection failure",
+        "Mobile application not syncing data",
+        "VoIP phone system issues",
+        "VPN connection problems for remote employees",
+        "System updates causing compatibility issues",
+        "File server running out of storage space",
+        "Intrusion detection system alerts",
+        "Inventory management system errors",
+        "Customer data not loading in CRM",
+        "Collaboration tool not sending notifications"
+    ]
+    return np.random.choice(issues)
+
+## Generate 100 rows of data
+data = {
+    'ID': ['TICKET-{}'.format(i) for i in range(1000, 1100)],
+    'Issue': [generate_issue() for _ in range(100)],
+    'Status': np.random.choice(['Open', 'In Progress', 'Resolved', 'Closed'], size=100),
+    'Priority': np.random.choice(['High', 'Medium', 'Low'], size=100)
+}
+
+## Create DataFrame
+df = pd.DataFrame(data)
+
+# Tabs for app layout
 tabs = st.tabs(['Write a ticket', 'Ticket Status and Analytics'])
 
 with tabs[0]:
@@ -32,6 +74,7 @@ with tabs[0]:
     submit = st.form_submit_button('Submit')
 
   if submit:
+      s = pd.Series()
       st.write('Ticket submitted!')
 
 with tabs[1]:
