644ec3c556aad885646ba2e28dbbac2ab478b2dc
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-20 16:45:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,19 +63,16 @@ with tabs[0]:
   st.write('File a new ticket')
 
   with st.form('addition'):
-    cost_center_list = ['HR001 Human Resources',
-                        'IT002 Information Technology',
-                        'MKT003 Marketing',
-                        'FIN004 Finance',
-                        'OPS005 Operations']
     issue = st.text_area('Description of issue')
-    cost_center_selection = st.selectbox('Cost center', cost_center_list)
-    deadline = st.date_input('Deadline', value=None)
+    priority = st.selectbox('Priority', ['High', 'Medium', 'Low'])
     submit = st.form_submit_button('Submit')
 
   if submit:
-      s = pd.Series()
+      df2 = pd.DataFrame({'Issue': issue,
+                        'Priority': priority})
       st.write('Ticket submitted!')
+      st.dataframe(df2)
 
 with tabs[1]:
   st.write('Check the status of your ticket')
+  st.dataframe(df)
