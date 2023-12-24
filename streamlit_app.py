7604056af95f12f41cecd45a71c35a177bf4205c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-24 22:37:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,8 +117,8 @@ with tabs[1]:
 
 
   # st.session_state.df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212, on_change=sort_df, 
-  st.session_state.df = st.session_state.df.sort_values(by=['Status', 'ID'], ascending=[False, False])
-  edited_df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212, on_change=sort_df,
+  st.button('Save', on_click=sort_df)
+  edited_df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212,
                 column_config={'Status': st.column_config.SelectboxColumn(
                                             'Status',
                                             help='Ticket status',
