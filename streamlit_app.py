2bf6ee0cab359dbe37aa003b919766246fe75ffa
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-24 22:38:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ if 'df' not in st.session_state:
 # Sort dataframe
 def sort_df():
     # st.session_state.df = st.session_state.df.sort_values(by=['Status', 'ID'], ascending=[False, False])
-    st.session_state.df = edited_df.copy()
+    st.session_state.df = edited_df.copy().sort_values(by=['Status', 'ID'], ascending=[False, False])
 
 
 # Tabs for app layout
