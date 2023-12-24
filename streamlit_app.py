c94f9c8f96c4b341d65cc71f20d68723371a82f1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-24 20:24:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ if 'df' not in st.session_state:
     st.session_state.df = df
 
 # Sort dataframe
-def sort_df(st.session_state.df):
+def sort_df():
     # st.session_state.df = st.session_state.df.sort_values(by=['Status', 'ID'], ascending=[False, False])
     st.session_state.df = st.session_state.df.sort_values(by=['Status', 'ID'], ascending=[False, False])
 
@@ -117,7 +117,7 @@ with tabs[1]:
 
 
   # st.session_state.df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212, on_change=sort_df, 
-  edited_df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212, on_change=sort_df, args=(st.session_state.df),
+  edited_df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212, on_change=sort_df),
                 column_config={'Status': st.column_config.SelectboxColumn(
                                             'Status',
                                             help='Ticket status',
