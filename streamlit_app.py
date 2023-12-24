aa57a187a16511b22940c48b8779c22c26d25963
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-12-24 23:28:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -119,6 +119,8 @@ with tabs[1]:
 
   # st.session_state.df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212, on_change=sort_df, 
   st.session_state.df.style.background_gradient(subset=['Status'])
+  st.session_state.df
+    
   edited_df = st.data_editor(st.session_state.df, use_container_width=True, hide_index=True, height=212,
                 column_config={'Status': st.column_config.SelectboxColumn(
                                             'Status',
