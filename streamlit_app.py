b8e3b91f06faa22b418b427105f073e6ac4ed0b3
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2024-01-11 08:50:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import altair as alt
 from datetime import datetime, timedelta
 
 # Page title
-st.set_page_config(page_title='Support Ticket Workflow', page_icon='🎫', layout='wide')
+st.set_page_config(page_title='Support Ticket Workflow', page_icon='🎫')
 st.title('🎫 Support Ticket Workflow')
 st.info('To write a ticket, fill out the form below. Check status or review ticketing analytics using the tabs below.')
 
